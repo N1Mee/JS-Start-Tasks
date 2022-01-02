395c2b04f802de417565d583cc1bc468674eab64
N1Mee <81901402+N1Mee@users.noreply.github.com>
2022-01-02 13:30:22 +0300

    Update task5.js

--- task5.js
+++ task5.js
@@ -6,15 +6,15 @@
 // ['name - Tom', 'age - 20', 'isMarried - false']
 // Реализовать это в виде функции, которая принимает объект и возвращает массив. Использовать цикл for-in
 
-// let user = { name: 'Tom', age: 20, isMarried: false }
-// function funct (obj) {
-//     let newarr = []
-//     for (let property in obj) {
-//         newarr.push(`${property} - ${obj[property]}`)
-//     }
-//     console.log(newarr)
-// }
-// funct(user)
+let user = { name: 'Tom', age: 20, isMarried: false }
+function funct (obj) {
+    let newarr = []
+    for (let property in obj) {
+        newarr.push(`${property} - ${obj[property]}`)
+    }
+    console.log(newarr)
+}
+funct(user)
 
 // 2).
 
@@ -27,20 +27,20 @@
 // sum(1); // введите два параметра
 // sum(); // введите два параметра
 
-// function sum (a,b) {
-//     let typea = typeof a
-//     let typeb = typeof b
-//     if (arguments.length < 2) {
-//         console.log('insert two numbers')
-//     }
-//     else if (typeof a != 'number' || typeof b != 'number'){
-//         console.log('not a number')
-//     }
-//     else {
-//         console.log(a+b)
-//     }
-// }
-// sum()
+function sum (a,b) {
+    let typea = typeof a
+    let typeb = typeof b
+    if (arguments.length < 2) {
+        console.log('insert two numbers')
+    }
+    else if (typeof a != 'number' || typeof b != 'number'){
+        console.log('not a number')
+    }
+    else {
+        console.log(a+b)
+    }
+}
+sum()
 
 // 3).
 
@@ -49,56 +49,56 @@
 // Функция создания случайного числа уже была ранее в материалах, в задаче по созданию случайного цвета.
 // Написать функцию в стрелочном синтаксисе.
 
-// function getRandomInteger(min, max) {
-//     return Math.floor(Math.random() * (max - min)) + min;
-//     }
-// let guess = (num) => {
-//     if (num < 0) {
-//         console.log('число должно быть не меньше 0')
-//     }
-//     else if (num > 10) {
-//         console.log('число должно быть не больше 10')
-//     }
-//     else {
-//         let a = getRandomInteger(0, 10);
-//         if (num === a) {
-//             console.log('Вы выиграли!')
-//         }
-//         else {
-//             console.log(`вы не угадали, ваше число ${num}, а выпало число ${a}`)
-//         }
-//     }
-// }
-// guess(3)
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+    }
+let guess = (num) => {
+    if (num < 0) {
+        console.log('число должно быть не меньше 0')
+    }
+    else if (num > 10) {
+        console.log('число должно быть не больше 10')
+    }
+    else {
+        let a = getRandomInteger(0, 10);
+        if (num === a) {
+            console.log('Вы выиграли!')
+        }
+        else {
+            console.log(`вы не угадали, ваше число ${num}, а выпало число ${a}`)
+        }
+    }
+}
+guess(3)
 
 // 4).
 
 // Напишите функцию copyArr(arr), которая копирует массив, не изменяя оригинал. Используйте подходящий метод массива.
 
-// newarr = ['bob', 'nikita', 'mike']
-// function copyArr(arr) {
-//     return arr.map(name => name)
-// }
-// console.log(copyArr(newarr))
+newarr = ['bob', 'nikita', 'mike']
+function copyArr(arr) {
+    return arr.map(name => name)
+}
+console.log(copyArr(newarr))
 
 // 5).
 
 // На странице в html задать список элементов li с числами. Джававскриптом нужно выбрать из этого списка четные числа и создать из них массив. Затем, используя метод forEach, создать из массива новый спсок элементов li и поместить эти элементы в исходный тег ul. Для обхода через querySelectorAll использовать цикл for-of (forEach работать не будет).
 
-// let listItems = document.querySelectorAll('li');
-// let ul = document.querySelector('ul');
-// let newarr = [];
-// for (let item of listItems) {
-//     if (item.innerText % 2 === 0) {
-//         newarr.push(item.innerText)
-//     }
-//     console.log(newarr)
-// }
-// newarr.forEach((item) => {
-//     let li = document.createElement('li')
-//     li.innerText = item;
-//     ul.appendChild(li)
-// })
+let listItems = document.querySelectorAll('li');
+let ul = document.querySelector('ul');
+let newarr = [];
+for (let item of listItems) {
+    if (item.innerText % 2 === 0) {
+        newarr.push(item.innerText)
+    }
+    console.log(newarr)
+}
+newarr.forEach((item) => {
+    let li = document.createElement('li')
+    li.innerText = item;
+    ul.appendChild(li)
+})
 
 // 6).
 
